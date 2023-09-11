66e18c601320f075024e2dbbf86d7885bf10f4ee
tometo <k.screw@icloud.com>
2023-09-11 22:46:07 +0900

    オーバーライド/上書き

---
+++ src/Test/Project/No402/Person02.java
@@ -0,0 +1,16 @@
+package Project.No402;
+
+public class Person02 {
+    private String name;
+
+    public void setName(String n){
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+    public void display(){
+        System.out.println("name : "+name);
+    }
+    
+}
---
+++ src/Test/Project/No402/StuSample02.java
@@ -0,0 +1,14 @@
+package Project.No402;
+
+public class StuSample02{
+
+     public static void main(String[] args){
+        Student02 stu = new Student02();
+        stu.setName("KAZUKI");    
+        stu.setStuNo(1);
+        stu.display();
+        }
+      
+      
+    
+}
