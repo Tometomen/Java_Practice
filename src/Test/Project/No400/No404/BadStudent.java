ecb4945ab2922384cba98f2e4349741a1a61536d
tometo <k.screw@icloud.com>
2023-09-16 10:37:19 +0900

    final定数

---
+++ src/Test/Project/No400/No404/BadStudent.java
@@ -0,0 +1,10 @@
+package Project.No400.No404;
+
+public class BadStudent /**extends Student4**/ {
+    
+    BadStudent(String name,int stuNo){
+      ///  super(name,stuNo);
+    }
+    //定数クラスから継承はできない。
+    
+}
