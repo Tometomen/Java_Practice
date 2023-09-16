8fce74add009c574007bf29a718096b38b86d04a
tometo <k.screw@icloud.com>
2023-09-16 10:58:49 +0900

    Taxi

---
+++ src/Test/Project/Test/T401/Car1.java
@@ -0,0 +1,14 @@
+package Project.Test.T401;
+
+public class Car1 {
+
+    private int no;
+
+    public void setNo(int no){
+        this.no = no;
+    }
+    public void display(){
+        System.out.println("Number : " + no);
+    }
+    
+}
---
+++ src/Test/Project/Test/T401/DriveTaxi.java
@@ -0,0 +1,13 @@
+package Project.Test.T401;
+
+public class DriveTaxi {
+
+    public static void main(String[] args) {
+        
+        Taxi1 t1 = new Taxi1();
+        t1.start();
+        t1.run();
+        t1.display();
+    }
+    
+}
